bc0827460b0039b769269d44030e7ab9c681eaab
Florian Mayer <flormayer@aim.com>
2011-04-07 14:44:10 +0200

    Add tests.

---
+++ burrahobbit/test/test_tree.py
@@ -0,0 +1,10 @@
+from burrahobbit._tree import DispatchNode, BitMapDispatch, ListDispatch
+
+def test_dispatch():
+    nd = BitMapDispatch()
+    for key in xrange(16):
+        nd = nd.replace(key, None)
+    assert isinstance(nd, BitMapDispatch)
+    nd = nd.replace(17, None)
+    assert isinstance(nd, ListDispatch)
+    
